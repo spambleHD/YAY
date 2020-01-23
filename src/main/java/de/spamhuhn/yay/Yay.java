375a1a17651ce047e7645a06fdeb0bb088603e14
spambleHD <konradsimlinger@gmail.com>
2020-01-23 19:49:28 +0100

    Initial commit

---
+++ src/main/java/de/spamhuhn/yay/Yay.java
@@ -0,0 +1,17 @@
+package de.spamhuhn.yay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Yay extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
